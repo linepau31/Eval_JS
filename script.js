d414ed1324f530852d199a4cf96299349c9343ba
Pauline Marot <pauline.marot2@outlook.fr>
2021-07-28 15:07:06 +0200

    ajout des chiffres aléatoire

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 // Url API : https://www.ericfreelance.fr/api/check_user.php
 
-const check_user = {
-    login: 'eric@free.fr',
-    password: '123456',
+function check_user() {
+
+    const logPass = 'eric@free.fr|123456';
+    return logPass;
+    
 }; 
  
-const options = {
-    method: 'POST',
-    body: JSON.stringify(check_user),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
- 
-fetch('https://www.ericfreelance.fr/api/check_user.php', options)
-    .then(res => res.json())
-    .then(res => console.log(res));
+const promise = fetch('https://www.ericfreelance.fr/api/check_user.php', {
+
+  method: 'POST',
+  body: JSON.stringify(check_user),
+  headers: {
+      'Content-Type': 'application/json'
+  },
+});
+
+promise.then(async (response) => {
+
+  try {
+    console.log(response);
+    const contenu = await response.json();
+    console.log(contenu);
+  } 
+  catch (e) {
+    console.log(e);
+  }
+});
+    
+
+function validation() {
+
+		let combinaison = logPass().split('|');
+		let login = combinaison[0];
+		let password = combinaison[1];
+
+		if (document.getElementById('login').value.toLowerCase() == login.toLowerCase() && document.getElementById('password').value == password) 
+		{
+			document.getElementById('message').innerText = "Vous êtes désormais connecté !"; // return true
+		}	
+		else {
+			document.getElementById('message').innerText = "L'authentification a échoué !"; // return false
+		}
+			
+	}
 
 const Form = document.getElementById('myForm');
-const Span = document.querySelector('.message');
-
-function validateEmail() {
-  let Email = document.forms["myForm"]["login"].value;
-  if (Email == "eric@free.fr") {
-    Span.innerHTML ="Vous êtes désormais connecté !";
-    return true;
-  } 
-
-  else (Email != "eric@free.fr") 
-  {
-    Span.innerHTML ="L’authentification a échoué !";
-    return false;
-  }  
-} 
-
-function validatePassword() {
-  let Password = document.forms["myForm"]["password"].value;
-  if (Password == "123456") {
-    Span.innerHTML ="Vous êtes désormais connecté !";
-    return true;
-  } 
-  else (Password != "123456") 
-  {
-    Span.innerHTML ="L’authentification a échoué !";
-    return false;
-  }
-}
 
 Form.addEventListener('submit', (e) => {
 
@@ -61,6 +61,7 @@ Form.addEventListener('submit', (e) => {
 
         e.preventDefault();
     } 
+
 
     const inpPassword = document.getElementById('password');
     let Regex = /^[1-9]+$/; // on accepte les chiffres de 1 à 9 répéter
@@ -83,16 +84,70 @@ Form.addEventListener('submit', (e) => {
 
 });
 
+/*
 
-// Fonction qui génère un nombre aléatoire
-const changeChif = function () {
-  // Générer un nombre entre 1 et 6
-  const nombreDecimal = (Math.random() * 9) + 1
-  const nombre = Math.trunc(nombreDecimal)
+// Les cellules
+let cel = document.querySelector('#cellule');
+let div = document.createElement('div');
+div.setAttribute('id', 'cases')
+div.style.width = '3,125rem';
+div.style.height = '3,125rem';
+div.style.justifyContent = 'center';
+div.style.display = 'grid';
+div.style.gridTemplateColumns = 'repeat(4, 4rem)';
+div.style.gridTemplateRows = 'repeat(4, 4rem)';
 
-  // Retourner ce nombre
-  return nombre
+for (let i = 0; i < 16; i++) {
+    let span = document.createElement('span');
+    span.style.border = '2px solid #ffffff';
+    span.style.background = '#e6e6e6';
+    span.style.cursor = 'pointer';
+    span.style.display = 'flex';
+    span.style.alignItems = 'center';
+    span.style.justifyContent = 'center';
+    div.appendChild(span);
 }
+
+cel.appendChild(div);  */
+
+
+generation();
+
+	function generation() {
+
+		let nb_alea;
+		let test = true; 
+		let chaine = "";
+
+		purger(); // permet de nettoyer la grille pour régenerer la grille
+
+		for (let i = 0; i < 10; i++) {
+
+			while (test == true) {
+				nb_alea = Math.floor(Math.random() * 16); // floor au dessus du chiffre
+				if (chaine.indexOf("-" + nb_alea + "-") > -1)
+				{
+					nb_alea = Math.floor(Math.random() * 16);
+				}	
+				else {
+					document.getElementById('case' + nb_alea).innerHTML = "<input type='button' value='" + i + "' onClick='document.getElementById(\"password\").value += " + i + "' />";
+					chaine += "-" + nb_alea + "-";
+					test = false;
+				}
+			}
+
+			test = true;
+		}
+	}
+
+	function purger() { // régenère la grille
+			for (let i = 0; i < 16; i++) {
+				document.getElementById('case' + i).innerHTML = ""; // réinitialise à vide ""
+			}
+			document.getElementById('login').value = "";
+			document.getElementById('password').value = "";
+	}
+
 
 
 
